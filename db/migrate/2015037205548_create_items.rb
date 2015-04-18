f134a3fa9ea19f0f3036ac11cdf596d92620cf94
micelinio <x14107660@student.ncirl.ie>
2015-04-18 12:42:14 +0000

    test123

---
+++ db/migrate/2015037205548_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    drop_table :items
+    create_table :items do |t|
+      t.string   :title
+      t.text     :description
+      t.decimal  :price      
+      t.string   :image_url
+   
+
+      t.timestamps null: false
+    end
+  end
+end
