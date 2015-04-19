16b72a0f5c2f7bbd3bf21b0cb2e0855aa37e0c37
micelinio <x14107660@student.ncirl.ie>
2015-04-19 08:58:15 +0000

    CHanged config.asets.compile to be true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'stormy-garden-9130.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  #config.serve_static_assets = true
-  #config.assets.compile = true
+  
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -34,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
